7d408a66a29120e445278106bb90042305450c12
Drayde <ed.v.morozov@gmail.com>
2024-10-27 14:31:05 +0100

    feat: completed task

--- homework02/mhd.c
+++ homework02/mhd.c
@@ -5,6 +5,7 @@ int isEven(int a) {
     return (a % 2 == 1) ? 0 : 1;
 }
 
+/*
 int findLCM(int a, int b) {
     int max = (a > b) ? a : b;
 
@@ -15,9 +16,18 @@ int findLCM(int a, int b) {
     }
     return lcm;
 }
+*/
+
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
 
 int main() {
-    // input for A
     int int1_h, int2_h, dep2_h;
     short int1_m, int2_m, dep2_m;
 
@@ -30,6 +40,11 @@ int main() {
 
     int int1 = int1_h * 60 + int1_m;
 
+    if (int1 == 0) { 
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
+
     printf("Interval spoje #2:\n");
     conversions = scanf("%d:%hd", &int2_h, &int2_m);
     if (conversions != 2 || int2_h < 0 || int2_m < 0 || int2_m >= 60) {
@@ -38,6 +53,11 @@ int main() {
     }
 
     int int2 = int2_h * 60 + int2_m;
+
+    if (int2 == 0) { 
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
 
     printf("Cas od odjezdu spoje #2:\n");
     conversions = scanf("%d:%hd", &dep2_h, &dep2_m);
@@ -48,7 +68,8 @@ int main() {
 
     int dep2 = dep2_h * 60 + dep2_m;
 
-    if (dep2 > int2 || dep2 == 0) {
+    //if (dep2 > int2 || dep2 == 0) {
+    if (dep2 >= int2) {
         printf("Nespravny vstup.\n");
         return 1;
     }
@@ -58,42 +79,48 @@ int main() {
 
     //printf("counting1 = %lld\ncounting2 = %lld\n", counting1, counting2);
 
-    if (isEven(int1_m) && isEven(int2_m) && isEven(dep2_m)) { 
-        printf("Spoj #2 nikdy nebude odjizdet 1 minutu po prijezdu spoje #1.\n");
-        return 0;
-    }
-
     if (0 == (counting2 - 1)) {
         printf("Spoj #2 bude odjizdet 1 minutu po prijezdu spoje #1 prave nyni.\n");
         return 0;
     }
 
-    int result = findLCM(int1, int2) - dep2;
+    // if both intervals are even numbers and if they are equal the transfer will never happen
+    if ((isEven(int1_m) && isEven(int2_m) && isEven(dep2_m)) || (int1_m == int2_m && int1_h == int2_h)) { 
+        printf("Spoj #2 nikdy nebude odjizdet 1 minutu po prijezdu spoje #1.\n");
+        return 0;
+    }
 
-    printf("Spoj #2 bude odjizdet 1 minutu po prijezdu spoje #1 za %d:%02d.\n", (result / 60), (result % 60));
+    int d = counting2 - counting1;
+    int gcd = findGCD(int1, int2);
+
+    if ((d - 1) % gcd != 0) {
+        printf("Spoj #2 nikdy nebude odjizdet 1 minutu po prijezdu spoje #1.\n");
+        return 0;
+    }
+
+    //int result = findLCM(int1, int2) - dep2;
+
+    //printf("Spoj #2 bude odjizdet 1 minutu po prijezdu spoje #1 za %d:%02d.\n", (result / 60), (result % 60));
 
 
-    /*
+    
     while (1) {
-
-        
         if (counting1 >= counting2) {
             counting2 += int2;
         } else {
             counting1 += int1;
         }
-        
 
-       
-        
+        //printf("counting1 = %d\ncounting2 = %d\n", counting1, counting2);
+        //iteration++;
+        //if (iteration > 50) break;
+
+
         if (counting1 == (counting2 - 1)) {
             printf("Spoj #2 bude odjizdet 1 minutu po prijezdu spoje #1 za %d:%02d.\n", (counting1 / 60), (counting1 % 60));
             break;
         }
-        
-
     }
-    */
     
 
     return 0;
