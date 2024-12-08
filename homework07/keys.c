526f9e95df930439003ad6d1a1150f5bb03c1983
Val <138670177+Val-On-Git@users.noreply.github.com>
2024-12-08 14:55:28 +0100

    fix: start revising

--- homework07/keys.c
+++ homework07/keys.c
@@ -7,6 +7,7 @@
 #define MAX_LEN 100
 
 int count = 0;
+int call_count = 0;
 
 int isProblemType (char o) {
     return !(o != '?' && o != '#');
@@ -44,11 +45,67 @@ void printSplit(const char *str, int *splitPositions, int splitCount) {
     putchar('\n');
 }
 
+int isValid(long long lastValue, long long nextValue) {
+    //return (lastValue % 2 == 0 && lastValue <= nextValue);
+    //printf("%lld <= %lld\n", lastValue, nextValue);
+    return (lastValue % 2 == 1 || lastValue <= nextValue);
+}
+
+int isValidTwo(const char *str, int *splitPositions, int splitCount, int index) {
+    
+    printf("splitCount = %d\n", splitCount);
+    
+    for (int i = 0; i < 4; i++) {
+        printf("splitPositions[%d] = %d\n", i, splitPositions[i]);
+    }
+    
+    printf("\n\n");
+    
+    //int start = splitPositions[0]+1;
+    int lastValue, nextValue;
+    
+    lastValue = getSectionValue(str, 0, splitPositions[0]);
+    
+    for (int i = 1; i < 3; i++) {
+        
+        if (splitPositions[i] == splitPositions[i+1]) break;
+        
+        //printf("start = %d\t", start);
+        //printf("splitPositions[%d] = %d\n", i, splitPositions[i]);
+        
+        //printf("splitPositions[%d] = %d (%d)\n", i, splitPositions[i], splitPositions[i]-2);
+        //printf("splitPositions[%d] = %d (%d)\n", i+1, splitPositions[i+1], splitPositions[i+1]-1);
+        
+        printf("splitPositions[%d] = %d (%d)\n", i, splitPositions[i], splitPositions[i]);
+        printf("splitPositions[%d] = %d (%d)\n", i+1, splitPositions[i+1], splitPositions[i+1]);
+        
+        
+        //nextValue = getSectionValue(str, start - 1, splitPositions[i]);
+        nextValue = getSectionValue(str, splitPositions[i], splitPositions[i+1]);
+        
+        printf("%lld <= %lld\n", lastValue, nextValue);
+        if (lastValue % 2 == 0 && lastValue > nextValue) {
+            printf("OOGA BOOGA!\n");
+        }
+        
+        
+        
+        lastValue = nextValue;
+        
+        
+        
+        //start = splitPositions[i] + 1;
+    }
+}
+
 // Recursive function to generate valid splits
 void generateSplits(const char *str, int *splitPositions, int splitCount, int index, int isVerbose) {
+    call_count++;
+    printf("#%d^\n", call_count);
     int n = strlen(str);
 
     if (index >= n) { 
+        printf("BOING\tsplitCount = %d\n", splitCount);
         count++;
         if (isVerbose) printSplit(str, splitPositions, splitCount);
         return;
@@ -63,12 +120,43 @@ void generateSplits(const char *str, int *splitPositions, int splitCount, int in
     int nextStart = index;
     long long nextValue = getSectionValue(str, nextStart, n);
 
+    printf("\t%lld->%lld\n", lastValue, nextValue);
+    /*
     if (lastValue % 2 == 1 || lastValue <= nextValue) {
+        printf("\tPassed. generateSplits splitCount %d, index %d\n", splitCount + 1, index + 1);
         generateSplits(str, splitPositions, splitCount + 1, index + 1, isVerbose);
+    } else {
+        printf("\tNOT passed\n");
     }
+    */
+    
+    //printf("\tisValid = %d\n", isValid(lastValue, nextValue));
+    if (index > 1) {
+    long long temp1 = getSectionValue(str, splitPositions[index-2], splitPositions[index-1]);
+    long long temp2 = getSectionValue(str, splitPositions[index-3], splitPositions[index-2]);
+    
+    //printf("\tindex = %d, splitPositions[index-2] = %d, splitPositions[index-1] = %d, temp1 = %lld\n", index, splitPositions[index-2], splitPositions[index-1], temp1);
+    //printf("\tindex = %d, splitPositions[index-3] = %d, splitPositions[index-2] = %d, temp2 = %lld\n", index, splitPositions[index-3], splitPositions[index-2], temp2);
+    }
+    
+    if (isValid(lastValue, nextValue)) {
+        printf("\tPassed. generateSplits splitCount %d, index %d\n", splitCount + 1, index + 1);
+        generateSplits(str, splitPositions, splitCount + 1, index + 1, isVerbose);
+    } else {
+        printf("\tNOT passed\n");
+    }
+    
+    //generateSplits(str, splitPositions, splitCount, index + 1, isVerbose);
 
     // Option 2: Do not split at this position
-    generateSplits(str, splitPositions, splitCount, index + 1, isVerbose);
+    
+    if (isValid(lastValue, nextValue)) {
+        isValidTwo(str, splitPositions, splitCount, index);
+        printf("\tUnconditional. generateSplits splitCount %d, index %d\n", splitCount, index + 1);
+        generateSplits(str, splitPositions, splitCount, index + 1, isVerbose);
+    }
+    
+    return;
 }
 
 void solve_problem(char* code, int isVerbose) {
@@ -76,7 +164,7 @@ void solve_problem(char* code, int isVerbose) {
 
     int len = strlen(code);
 
-    int *splitPositions = (int *)malloc(len * sizeof(int));
+    int *splitPositions = (int *)malloc((len-1) * sizeof(int));
 
     count = 0;
     generateSplits(code, splitPositions, 0, 1, isVerbose);
